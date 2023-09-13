4b6eafebbb11d19d473152b77e7b5cad9648afa2
Enzo <enzolozano90@gmail.com>
2023-09-13 20:35:15 -0300

    arquivos adicionados

---
+++ equipe4/app/Components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <section>
+        <div>
+          <h1>home</h1>
+          <h1>sobre nos</h1>
+          <h1>etc</h1>
+        </div>
+      </section>
+    )}
---
+++ equipe4/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <section>
+      <div>
+        <h1>Oi</h1>
+        <Navbar/>
+      </div>
+    </section>
+  )}
